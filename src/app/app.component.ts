360a059d17619abd1e20bad0db06dee9bec7ea7e
88pawankumarjha <88pawankumarjha@gmail.com>
2019-03-20 20:35:27 +0530

    7 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataServiceService } from './data-service.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'my-app',
@@ -7,12 +8,25 @@ import { DataServiceService } from './data-service.service';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  
+  theResult;
+  resultMessage;
   firstLoad=true;
   showOtherCard=false;
   myCard;
   otherCard;
-  constructor(private dataServiceService: DataServiceService){
+  constructor(private dataServiceService: DataServiceService, private toastr: ToastrService){
   }
+
+  showSuccess() {
+    this.toastr.success(this.resultMessage, 'You\'re the winner!!',
+    {timeOut: 2000, positionClass: 'toast-center-center' });
+  }
+  showError() {
+    this.toastr.error(this.resultMessage, 'You lose.', {
+    timeOut: 2000, positionClass: 'toast-center-center' })
+  }
+
   showCard(){
     this.firstLoad=false;
     this.showOtherCard=false;
@@ -24,26 +38,38 @@ export class AppComponent  {
 
   toggleOtherCard(index){
     
+    const myCardLabel = this.dataServiceService.myCard[index].split(":")[0];
+    const myCardValue = this.dataServiceService.myCard[index].split(":")[1];
+    const otherCardValue = this.dataServiceService.otherCard[index].split(":")[1];
+
     this.showOtherCard=true;
     setTimeout(() => {
-        if(this.dataServiceService.myCard[index].split(":")[0] != "Rank") {
-          if(+this.dataServiceService.myCard[index].split(":")[1] >= +this.dataServiceService.otherCard[index].split(":")[1]) {
-            alert(this.dataServiceService.myCard[index].split(":")[0]+ " " + this.dataServiceService.myCard[index].split(":")[1]+ " WIN " + this.dataServiceService.otherCard[index].split(":")[1]);
-          }else{
-            alert(this.dataServiceService.myCard[index].split(":")[0]+ " " + this.dataServiceService.myCard[index].split(":")[1]+ " LOST " + this.dataServiceService.otherCard[index].split(":")[1])
-          }
+        if(myCardLabel != "Rank") {
+          +myCardValue >= +otherCardValue?
+            this.announceResult(1, myCardLabel, myCardValue, otherCardValue):
+            this.announceResult(2, myCardLabel, myCardValue, otherCardValue)
         } else{
-          if(+this.dataServiceService.myCard[index].split(":")[1] >= +this.dataServiceService.otherCard[index].split(":")[1]) {
-            alert(this.dataServiceService.myCard[index].split(":")[0]+ " " + this.dataServiceService.myCard[index].split(":")[1]+ " LOST " + this.dataServiceService.otherCard[index].split(":")[1]);
-          }else{
-            alert(this.dataServiceService.myCard[index].split(":")[0]+ " " + this.dataServiceService.myCard[index].split(":")[1]+ " WIN " + this.dataServiceService.otherCard[index].split(":")[1])
-          }
+          +myCardValue >= +otherCardValue?
+            this.announceResult(2, myCardLabel, myCardValue, otherCardValue):
+            this.announceResult(1, myCardLabel, myCardValue, otherCardValue)
         }
-        
     }, 1000);
+
     setTimeout(() => {
       this.hideCard();
     }, 2000);
 
+  }
+  
+  announceResult(number, label, v1, v2){
+    if(number==1){
+      this.theResult=0;
+      this.resultMessage = label+ " " + v1+ " WIN " + label+ " " +v2;
+      this.showSuccess();
+    }else{
+      this.theResult=1
+      this.resultMessage = label+ " " + v1+ " LOST " + label+ " " + v2;
+      this.showError();
+    }
   }
 }

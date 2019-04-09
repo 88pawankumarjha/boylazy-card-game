59382ee18ef3cf59677d98635f1a7094c2b4fa8c
88pawankumarjha <88pawankumarjha@gmail.com>
2019-04-09 18:26:37 +0530

    QA check for service file

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -1,92 +1,93 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse } from '@angular/common/http';
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
 export class DataServiceService {
-  
-  selCardField =0;
-  myCard = ['Name:RomanReigns', 'Rank:1', 'Height:6.3', 'Weight:250', 'Fights Won:235'];
-  otherCard = ['Name:SamoaJoe', 'Rank:2', 'Height:6.3', 'Weight:350', 'Fights Won:95'];
-  arrCards;
-  myCard1;
-  otherCard1;
-  myArrCards;
-  otherArrCards;
-  half_length=0;
-  constructor (private httpService: HttpClient) { 
+
+  private zero = 0;
+  private selCardField = 0;
+  private myCard = ['Name:RomanReigns', 'Rank:1', 'Height:6.3', 'Weight:250', 'Fights Won:235'];
+  private otherCard = ['Name:SamoaJoe', 'Rank:2', 'Height:6.3', 'Weight:350', 'Fights Won:95'];
+  private arrCards;
+  private myCard1;
+  private otherCard1;
+  private myArrCards;
+  private otherArrCards;
+  private half_length = 0;
+
+  constructor(private httpService: HttpClient) {
     this.initApp();
   }
-  initApp(){
+  private initApp() {
     this.httpService.get('./assets/cards.json').subscribe(
       data => {
-        this.arrCards = data as string [];
+        this.arrCards = data as string[];
         this.shuffleAndDistributeCards();
       },
       (err: HttpErrorResponse) => {
-        console.log (err.message);
-      }
+        console.log(err.message);
+      },
     )
   }
-  setSelCardField(index){
+  private setSelCardField(index) {
     this.selCardField = index;
     setTimeout(() => {
       this.selCardField = 0;
-      this.myArrCards.splice(0,1);
-      this.otherArrCards.splice(0,1);
+      this.myArrCards.splice(0, 1);
+      this.otherArrCards.splice(0, 1);
     }, 2000);
   }
-  getSelCardField(){
+  private getSelCardField() {
     return this.selCardField;
   }
-  
-  fetchMyCard(){
-    if(undefined != this.myArrCards &&this.myArrCards.length != 0 )
-    return this.myArrCards[0].URL;
-    else 
-    return "#";
+
+  private fetchMyCard() {
+    if (undefined != this.myArrCards && this.myArrCards.length != this.zero)
+      return this.myArrCards[this.zero].URL;
+    else
+      return "#";
   }
-  fetchOtherCard(){
-    if(undefined != this.otherArrCards &&this.otherArrCards.length != 0)
-    return this.otherArrCards[0].URL;
-    else 
-    return "https://shop.wwe.com/on/demandware.static/-/Sites-wwe-us-navigation/default/dwb8d1545f/images/superstar-thumb-300/RomanReignsNEW.jpg"
+  private fetchOtherCard() {
+    if (undefined != this.otherArrCards && this.otherArrCards.length != this.zero)
+      return this.otherArrCards[this.zero].URL;
+    else
+      return
+      "https://bit.ly/2U5IUkj"
   }
-  
-  getMyCard(){
+
+  private getMyCard() {
     return this.myCard1;
   }
-  getOtherCard(){
+  getOtherCard() {
     return this.otherCard1;
   }
 
-  shuffleAndDistributeCards(){
+  private shuffleAndDistributeCards() {
     this.myArrCards = this.shuffle(this.arrCards);
-    this.half_length = Math.ceil(this.myArrCards.length / 2);    
-    this.otherArrCards = this.myArrCards.splice(0,this.half_length);
+    this.half_length = Math.ceil(this.myArrCards.length / 2);
+    this.otherArrCards = this.myArrCards.splice(this.zero, this.half_length);
   }
 
-shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;  
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+  private shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (this.zero !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
   }
-  return array;
-}
 
-openURL(stringURL){
+  private openURL(stringURL) {
     window.open(stringURL, "_blank");
-}
+  }
 
 
 }

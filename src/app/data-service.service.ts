5b469c10ae8896f795c927fd8d96fc4ccb20edc5
88pawankumarjha <88pawankumarjha@gmail.com>
2019-04-09 19:03:47 +0530

    QA changes for data service class

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -6,9 +6,11 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 })
 export class DataServiceService {
 
-  private zero = 0;
-  private delay = 2000;
-  private selCardField = this.zero;
+  private zero: number = 0;
+  private delay: number = 2000;
+  private selCardField: number = this.zero;
+  private half_length: number = this.zero;
+
   private myCard: string[] = ['Name:RomanReigns', 'Rank:1', 'Height:6.3', 'Weight:250', 'Fights Won:235'];
   private otherCard: string[] = ['Name:SamoaJoe', 'Rank:2', 'Height:6.3', 'Weight:350', 'Fights Won:95'];
   private arrCards: string[];
@@ -16,7 +18,6 @@ export class DataServiceService {
   private otherCard1: string[];
   private myArrCards: string[];
   private otherArrCards: string[];
-  private half_length = this.zero;
 
   constructor(private httpService: HttpClient) {
     this.initApp();
@@ -43,7 +44,6 @@ export class DataServiceService {
   private getSelCardField(): number {
     return this.selCardField;
   }
-
   private fetchMyCard(): string {
     if (undefined != this.myArrCards && this.myArrCards.length != this.zero)
       return this.myArrCards[this.zero].URL;
@@ -57,27 +57,23 @@ export class DataServiceService {
       return
       "https://bit.ly/2U5IUkj"
   }
-
   private getMyCard(): Array<string>{
     return this.myCard1;
   }
   private getOtherCard(): Array<string>{
     return this.otherCard1;
   }
-
   private shuffleAndDistributeCards(): void {
     this.myArrCards = this.shuffle(this.arrCards);
     this.half_length = Math.ceil(this.myArrCards.length / 2);
     this.otherArrCards = this.myArrCards.splice(this.zero, this.half_length);
   }
-
   private shuffle(array: Array<string>): Array<string> {
     var currentIndex = array.length, temporaryValue, randomIndex;
     while (this.zero !== currentIndex) {
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
@@ -85,10 +81,7 @@ export class DataServiceService {
     }
     return array;
   }
-
   private openURL(stringURL: string): void {
     window.open(stringURL, "_blank");
   }
-
-
 }
